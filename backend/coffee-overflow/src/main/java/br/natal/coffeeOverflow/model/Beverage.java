4aa95e985b9e68f151b6916d56f71adadc92563c
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-09 08:12:10 -0300

    feat(backend): abstract class Beverage

---
+++ backend/coffee-overflow/src/main/java/br/natal/coffeeOverflow/model/Beverage.java
@@ -0,0 +1,42 @@
+package br.natal.coffeeOverflow.model;
+
+import jakarta.persistence.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Entity
+public abstract class Beverage implements AbstractBeverage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
