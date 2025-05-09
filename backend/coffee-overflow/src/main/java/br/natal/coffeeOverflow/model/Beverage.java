b30e93f4531a6ef5cd9ff2895f00b5db14d1acd8
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-09 08:28:38 -0300

    feat(backend): Inheritance annotation on Beverage abstract class

--- backend/coffee-overflow/src/main/java/br/natal/coffeeOverflow/model/Beverage.java
+++ backend/coffee-overflow/src/main/java/br/natal/coffeeOverflow/model/Beverage.java
@@ -5,8 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class Beverage implements AbstractBeverage {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
