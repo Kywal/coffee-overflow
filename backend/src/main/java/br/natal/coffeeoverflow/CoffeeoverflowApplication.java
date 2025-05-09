22055c0cce4b6f9f7268fe274fb73a056e65ae11
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-09 07:28:40 -0300

    Initial commit

---
+++ backend/src/main/java/br/natal/coffeeoverflow/CoffeeoverflowApplication.java
@@ -0,0 +1,13 @@
+package br.natal.coffeeoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeoverflowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/natal/coffeeoverflow/CoffeeoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.natal.coffeeoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeoverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
