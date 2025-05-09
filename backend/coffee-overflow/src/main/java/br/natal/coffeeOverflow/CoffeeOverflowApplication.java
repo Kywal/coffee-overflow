4ebac73173df6a72fd8c7d656e6476a25c754e4f
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-09 07:37:39 -0300

    build: update backend project

---
+++ backend/coffee-overflow/src/main/java/br/natal/coffeeOverflow/CoffeeOverflowApplication.java
@@ -0,0 +1,13 @@
+package br.natal.coffeeOverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeOverflowApplication.class, args);
+	}
+
+}
---
+++ backend/coffee-overflow/src/test/java/br/natal/coffeeOverflow/CoffeeOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.natal.coffeeOverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
